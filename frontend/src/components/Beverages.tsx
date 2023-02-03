66567241581a3b5b2b80d438c653d27f996e55bb
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2023-02-03 22:31:34 +0200

    Add usage of beverage.css

--- frontend/src/components/Beverages.tsx
+++ frontend/src/components/Beverages.tsx
@@ -1,6 +1,7 @@
 import useBeverages from "../hooks/useBeverages"
 import { BeverageType } from "../types"
 import Beverage from "./Beverage"
+import "../Beverage.css"
 
 function Beverages() {
   const beverages = useBeverages()
@@ -8,7 +9,6 @@ function Beverages() {
   function splitBeverages(
     beverages: BeverageType[]
   ): [BeverageType[], BeverageType[]] {
-    
     return beverages.reduce(
       ([coffee, tea], beverage) => {
         return beverage.beverageType === "Coffee"
@@ -22,15 +22,19 @@ function Beverages() {
   const [coffees, teas] = splitBeverages(beverages)
 
   return (
-    <div>
-      <h2>Coffees</h2>
-      {coffees.map((coffee, index) => (
-        <Beverage key={index} beverage={coffee} />
-      ))}
-      <h2>Teas</h2>
-      {teas.map((tea, index) => (
-        <Beverage key={index} beverage={tea} />
-      ))}
+    <div className="gridWrapper">
+      <div>
+        <h2>Coffees</h2>
+        {coffees.map((coffee, index) => (
+          <Beverage key={index} beverage={coffee} />
+        ))}
+      </div>
+      <div>
+        <h2>Teas</h2>
+        {teas.map((tea, index) => (
+          <Beverage key={index} beverage={tea} />
+        ))}
+      </div>
     </div>
   )
 }
