cbe9605bb9670b19d8b8ef5ef3ac1ed86b9aef5a
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2023-02-03 23:34:06 +0200

    Update validation requirements

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const BeverageZod = z.object({
-  name: z.string(),
-  weight: z.number().int(),
-  price: z.number(),
+  name: z.string().min(1),
+  weight: z.number().int().positive(),
+  price: z.number().positive(),
   roastLevel: z.number().int().min(1).max(5),
   beverageType: z.enum(['Coffee', 'Tea']),
 })
