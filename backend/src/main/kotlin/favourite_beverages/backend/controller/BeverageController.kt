0a9933958b3a7c38c96085cec35e284193017ac7
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2023-02-03 21:40:19 +0200

    Add support for post requests

--- backend/src/main/kotlin/favourite_beverages/backend/controller/BeverageController.kt
+++ backend/src/main/kotlin/favourite_beverages/backend/controller/BeverageController.kt
@@ -1,5 +1,6 @@
 package favourite_beverages.backend.controller
 
+import favourite_beverages.backend.service.Beverage
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -8,15 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import favourite_beverages.backend.service.BeverageService
+import org.springframework.validation.annotation.Validated
 
 @RequestMapping("/api/beverages")
 @RestController
+@Validated
 class BeverageController(val service: BeverageService) {
     @GetMapping
     fun getBeverages() = service.getBeverages()
 
-    //@PostMapping
-    //@ResponseStatus(HttpStatus.CREATED)
-    //fun saveBeverage(@RequestBody beverage: BeverageController): BeverageController = //service.create(beverage)
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun saveBeverage(@RequestBody beverage: Beverage): Beverage = service.createBeverage(beverage)
 }
--- backend/src/main/kotlin/favourite_beverages/backend/service/BeverageService.kt
+++ backend/src/main/kotlin/favourite_beverages/backend/service/BeverageService.kt
@@ -3,6 +3,8 @@ package favourite_beverages.backend.service
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import org.springframework.stereotype.Service
+import java.io.File
+import java.io.FileWriter
 
 var gson = Gson()
 
@@ -20,14 +22,32 @@ data class Beverage(
 
 @Service
 class BeverageService {
-
+    val fileName = "beverages.json"
+    val fileLocation = "./src/main/resources/"
     fun getBeverages(): List<Beverage> {
-        val beveragesJSON = this::class.java.classLoader.getResource("beverages.json")?.readText()
+        val file = File(fileLocation + fileName)
+        val beveragesJSON =  file.readText()
         println(beveragesJSON)
+
+        if (beveragesJSON.isEmpty()) return emptyList()
 
         return gson.fromJson(beveragesJSON,
             object : TypeToken<List<Beverage>>() {}.type
         )
+    }
+
+    fun createBeverage(beverage: Beverage): Beverage {
+
+           val beverages = getBeverages()
+
+           val json = gson.toJson(beverages + beverage)
+           println(json)
+
+           val writer = FileWriter(fileLocation + fileName)
+           writer.write(json)
+           writer.close()
+
+           return beverage
     }
 
 }
